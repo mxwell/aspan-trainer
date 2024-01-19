61b9f630c11b2de14c8048a4058198afe624117e
Murat Khairulin <murat.khairulin@gmail.com>
2024-01-19 23:34:44 +0600

    [viewer] Fix tailwind pruning

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 module.exports = {
     purge: [
+        "./static/src/html/partials/viewer_page.pug",
         "./static/src/html/viewer.pug",
+        "./static/src/html/viewer_en.pug",
         "./static/src/javascripts/components/viewer_app.jsx",
         "./static/src/javascripts/components/side_quiz.jsx",
     ],
