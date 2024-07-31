6a0f8ef8885944e4bfaa8de48fec8e9b7fb21468
Murat Khairulin <murat.khairulin@gmail.com>
2024-07-31 19:32:56 +0500

    [gc] Add a separate base template for HTML

--- static/src/javascripts/gc_create_ru.jsx
+++ static/src/javascripts/gc_create_ru.jsx
@@ -1,11 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { I18N_LANG_RU, initUiLangSwitcher } from './lib/i18n';
-import { initViewerMenuButton } from './lib/viewer_menu';
+import { I18N_LANG_RU } from './lib/i18n';
 import GcCreateApp from './components/gc_create_app';
-
-initViewerMenuButton();
-initUiLangSwitcher();
 
 const root = document.getElementById("gc_create_root");
 
--- static/src/javascripts/gc_search_ru.jsx
+++ static/src/javascripts/gc_search_ru.jsx
@@ -1,11 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import GcSearchApp from './components/gc_search_app';
-import { I18N_LANG_RU, initUiLangSwitcher } from './lib/i18n';
-import { initViewerMenuButton } from './lib/viewer_menu';
-
-initViewerMenuButton();
-initUiLangSwitcher();
+import { I18N_LANG_RU } from './lib/i18n';
 
 const root = document.getElementById("gc_search_root");
 
